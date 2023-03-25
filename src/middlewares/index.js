628f48772d30a9a9d2d89ca83619a0484118a726
D-Wolter <wolter.daniel@gmail.com>
2023-03-25 19:51:13 -0300

    feat: remoção de middlewares não usados

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,19 +1,11 @@
 const { verifyId } = require('./verifyId');
-const { verifyName } = require('./verifyName');
-const { verifyProductId } = require('./verifyProductId');
-const { verifyQuantity } = require('./verifyQuantity');
 const { schemas } = require('./schemas');
-const { verifyInsertProduct } = require('./verifyInsertProduct');
 const { validateIdBySaleProduct } = require('./validateIdBySaleProduct');
 const { validateSale } = require('./validate.sale');
 
 module.exports = {
   verifyId,
-  verifyName,
-  verifyProductId,
-  verifyQuantity,
   schemas,
-  verifyInsertProduct,
   validateIdBySaleProduct,
   validateSale,
 };
