dcab8266e13b8d2bb7ce54649be326103c302973
D-Wolter <wolter.daniel@gmail.com>
2023-03-28 16:08:30 -0300

    feat: teste sales service caso erro busca por id

--- tests/unit/services/mocks/sales.service.mock.js
+++ tests/unit/services/mocks/sales.service.mock.js
@@ -17,13 +17,8 @@ const sales = [
 
 const findSaleId = [
   {
-    "date": "2021-09-09T04:54:29.000Z",
-    "productId": 1,
-    "quantity": 2
-  },
-  {
     "date": "2021-09-09T04:54:54.000Z",
-    "productId": 2,
+    "productId": 3,
     "quantity": 2
   }
 
--- tests/unit/services/sales.service.test.js
+++ tests/unit/services/sales.service.test.js
@@ -2,21 +2,33 @@ const { expect } = require('chai');
 const sinon = require('sinon');
 
 const { salesModel } = require('../../../src/models');
-const { salesService, productsService } = require('../../../src/services/');
-const { allSales } = require('./mocks/sales.service.mock');
+const { salesService } = require('../../../src/services/');
+const { allSales, findSaleId } = require('./mocks/sales.service.mock');
 
 describe('Testes de unidade de sales na camada service', () => {
   it('teste getAllSales na service (caso de sucesso)', async () => {
 
-      sinon.stub(salesModel, 'getAllSales')
-      .resolves(allSales);
+    sinon.stub(salesModel, 'getAllSales').resolves(allSales);
     
     const result = await salesService.getAllSales()
-    console.log('++++++++++', result)
-
 
     expect(result.message).to.be.deep.equal(allSales)
   });
+  it('teste getSalesById na service (caso de sucesso)', async function () {
+    
+    sinon.stub(salesModel, 'findByIdSales').resolves(findSaleId);
+    
+    const result = await salesService.findIdExist(2)
+    
+    expect(result.message).to.be.deep.equal(findSaleId)
+  })
+  it('teste getSalesById na service (caso de erro id nao encontrado)', async () => {
+    
+    const result = await salesService.findIdExist(999)
+    
+    expect(result.type).to.be.equal('INVALID VALUE');
+    expect(result.message).to.be.deep.equal('Sale not found')
+  })
 
   afterEach(sinon.restore);
 });
