90c9ac2356ee6afa771b5e4f62c3e21f1a7cc1f4
D-Wolter <wolter.daniel@gmail.com>
2023-03-28 13:07:16 -0300

    feat: add teste em sales controler getall

--- tests/unit/controllers/mocks/sales.controller.mock.js
+++ tests/unit/controllers/mocks/sales.controller.mock.js
@@ -1,3 +1,18 @@
+const allSales = [
+  {
+    saleId: 1,
+    date: "2021-09-09T04:54:29.000Z",
+    productId: 1,
+    quantity: 2
+  },
+  {
+    saleId: 1,
+    date: "2021-09-09T04:54:54.000Z",
+    productId: 2,
+    quantity: 2
+  }
+]
+
 const salesError = {
   type: 'PRODUCT_NOT_FOUND',
   message: 'Product not found',
@@ -18,6 +33,7 @@ const salesSucess = {
 };
 
 module.exports = {
+  allSales,
   salesError,
   salesSucess,
 }
--- tests/unit/controllers/sales.controller.test.js
+++ tests/unit/controllers/sales.controller.test.js
@@ -6,9 +6,24 @@ chai.use(sinonChai);
 
 const { salesController } = require('../../../src/controllers');
 const { salesService } = require('../../../src/services');
-const { salesError, salesSucess } = require('./mocks/sales.controller.mock');
+const { allSales, salesError, salesSucess } = require('./mocks/sales.controller.mock');
 
 describe('Testes de unidade de sales, na camada controller', () => {
+    it('teste de Busca por todas as vendas (caso de sucesso)', async () => {
+    const res = {};
+    const req = {}
+
+    res.status = sinon.stub().returns(res);
+    res.json = sinon.stub().returns();
+    sinon
+      .stub(salesService, 'getAllSales')
+      .resolves({ type: null, message: allSales });
+
+      await salesController.getAllSales(req, res);
+
+      expect(res.status).to.have.been.calledWith(200);
+      expect(res.json).to.have.been.calledWith(allSales);
+    });
   it('teste de inserção de nova venda (caso de sucesso)', async () => {
     const res = {};
     const req = { body: salesSucess.itemsSold }
